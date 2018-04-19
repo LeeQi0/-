fb582a7886b4722eed390158a2693640c880ebfe
LiqiQi0 <451117877@QQ.COM>
2018-04-18 20:50:55 -0500

    Create request.py

---
+++ request.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+import urllib.request
+ 
+
+response = urllib.request.urlopen('http://52./')
+html = response.read()
+root = ET.fromstring(html)
+if root[0].text=="a"
+   
+if root[0].text=="b"
+
+if root[0].text=="c"
+
+if root[0].text=="d"
+
+if root[0].text=="e"
+
+if root[0].text=="f"
