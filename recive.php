f0ee3a3d43a8a48799438ae2b6f5c34b874f6551
LiqiQi0 <451117877@QQ.COM>
2018-04-18 21:09:06 -0500

    Update recive.php

--- recive.php
+++ recive.php
@@ -1,5 +1,31 @@
 <?php
-  $number=$_POST["Channel"];
+  $charact=$_POST["Channel"];
   $urlLink=$_POST["url"];
- 
+  $xml=simplexml_load_file("note.xml") or die("Error: Cannot create object");
+  $xml->whether=$charact;
+  if($charact=="a")
+  {$xml->Channel=}
+  if($charact=="b")
+  {}
+  if($charact=="c")
+  {}
+  if($charact=="d")
+  {}
+ if($charact=="e")
+  {}
+ if($charact=="f")
+  {}
+  $xml->asXML();
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $xml);
+  fclose($myfile);
+
+  sleep(15);//sleep 15 sec
+
+  $xml=simplexml_load_file("note.xml") or die("Error: Cannot create object");
+  $xml->whether="no";
+  $xml->asXML();
+  $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+  fwrite($myfile, $xml);
+  fclose($myfile);
  ?>
